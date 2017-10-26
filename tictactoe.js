b98f046ada103a273fd78965b9c033c7a2ae6414
Liana Mancini <lianamancini@Lianas-MacBook-Pro.local>
2017-10-26 18:20:56 -0400

    fixed bug to allow symbol choice after game win

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,9 @@
-// TODO: styling
 // TODO: let player choose X or O
 // TODO: computer strategy
 // BONUS: two players?
 
 $(document).ready(function () {
     //console.log("Document ready");
-
-    // var thex = document.getElementById("x");
-    // thex.innerHTML = "WELL THIS WORKED";
-    // document.getElementById("x").innerHTML = "XTOBER";
-    
 
     // could this be tighter with an object?
     var topL = document.getElementById("top-L");
@@ -22,6 +16,7 @@ $(document).ready(function () {
     var bottomM = document.getElementById("bottom-M");
     var bottomR = document.getElementById("bottom-R");
 
+    // array of DOM nodes for the board
     var squares = [topL, topM, topR, midL, midM, midR, bottomL, bottomM, bottomR];
 
     var playerTurns = [];
@@ -35,6 +30,9 @@ $(document).ready(function () {
     var playerSymbol;
     var compSymbol;
 
+    // matches to win per series before reset
+    const GAME_WIN = 1;
+
     // toggles lock from player clicks during timeout before computer move
     var lockout = false;
 
@@ -47,7 +45,50 @@ $(document).ready(function () {
 
     // sets game end message
     var message = document.getElementById("message");
-    
+
+    // access space-savers object
+    // an array of dom nodes that are all spans
+    var savers = document.getElementsByClassName("saver");
+
+    // toggles play again button
+    var playAgainButton = document.getElementById("play-again");
+
+    var firstMatch;
+
+    // called at the end of each match and before the next
+    function activateBoard(){
+        console.log("game is ready to play");
+
+        lockout = false;
+
+        // clear the match outcome message
+        message.innerHTML = "Best 2 out of 3 WINS!"; 
+
+        for (var i = 0; i < squares.length; i++) {
+            //console.log("Loop running! i is " + i);
+            console.log("event listener added to " + squares[i]);
+            squares[i].addEventListener("click", playerMove);
+        } 
+        return true;
+    }
+
+    // resets the game after a game win
+    function playAgain(){
+
+        firstMatch = true;
+        // reset scoreboard
+        playerScore = 0;
+        compScore = 0;
+        playerScoreBoard.innerHTML = playerScore;
+        compScoreBoard.innerHTML = compScore;
+
+        console.log("play again button clicked");
+
+        clearBoard();
+
+        // hide play again button
+        playAgainButton.style.visibility = "hidden";
+    }
 
     // allow player to choose X or O
     function assignX() {
@@ -55,6 +96,9 @@ $(document).ready(function () {
         compSymbol = "O";
         topL.removeEventListener("click", assignX);
         topR.removeEventListener("click", assignO);
+        document.getElementById("x").style.visibility = "hidden";
+        document.getElementById("o").style.visibility = "hidden";
+
         clearBoard();
     }
 
@@ -63,37 +107,12 @@ $(document).ready(function () {
         compSymbol = "X";    
         topL.removeEventListener("click", assignX);
         topR.removeEventListener("click", assignO);
+        document.getElementById("x").style.visibility = "hidden";
+        document.getElementById("o").style.visibility = "hidden";
         clearBoard();   
     }
 
-
-    // access space-savers object
-    var savers = document.getElementsByClassName("saver");
-
-    // toggles play again button
-    var playAgainButton = document.getElementById("play-again");
-
-    // resets the game after a game win
-    function playAgain(){
-        // reset scoreboard
-        playerScore = 0;
-        compScore = 0;
-        playerScoreBoard.innerHTML = playerScore;
-        compScoreBoard.innerHTML = compScore;
-
-        // reset board and allow player symbol choice
-        document.getElementById("x").innerHTML = "X";
-        document.getElementById("o").innerHTML = "O";
-        topL.addEventListener("click", assignX);
-        topR.addEventListener("click", assignO);
-
-        // hide play again button
-        playAgainButton.style.visibility = "hidden";
-    }
-
-    playAgainButton.addEventListener("click", playAgain);
-
-    function clickEvent(){
+    function playerMove(){
         // if locked out, function does not execute
         if (lockout === true){
             return;
@@ -107,7 +126,7 @@ $(document).ready(function () {
         //console.log("I got clicked!" + this.id);
         playerTurns.push(this.id);
         //console.log("PLayer turns so far: " + playerTurns);
-        this.removeEventListener("click", clickEvent);
+        this.removeEventListener("click", playerMove);
 
         // lock the board from clicks after player's move
         lockout = true;
@@ -131,12 +150,13 @@ $(document).ready(function () {
     function endGame(){
         // deactivate board by removing all event listeners
         for (var i = 0; i < squares.length; i++) {
-            squares[i].removeEventListener("click", clickEvent);
+            squares[i].removeEventListener("click", playerMove);
+            console.log("event listener removed from " + squares[i]);
         }
-        console.log("match over");
+        //console.log("match over");
 
         // do not clear the board if there has been a game win
-        if (playerScore === 3 || compScore === 3){
+        if (playerScore === GAME_WIN || compScore === GAME_WIN){
             return;
         } else {
             // clear the board
@@ -144,30 +164,67 @@ $(document).ready(function () {
         }
     }
 
+    function chooseSymbol() {
+        console.log("choose symbol");
+        // if this is the first match, let player choose symbol 
+        message.innerHTML = "Choose X or O!<br />(X moves first)";
+
+        // DOM traversal to access first child or something of a saver span and adds the x and o id back
+        topL.firstChild.setAttribute("id", "x");
+        topL.firstChild.innerHTML = "X";
+        topR.firstChild.setAttribute("id", "o");
+        topR.firstChild.innerHTML = "O";
+        
+        //console.log(document.getElementById("top-L").firstChild);
+
+        // reset board and allow player symbol choice
+        //document.getElementById("o").innerHTML = "O";
+        topL.addEventListener("click", assignX);
+        topR.addEventListener("click", assignO);
+
+        //topL.innerHTML = "<span class=\"saver\">.<\/span>";
+        //topR.innerHTML = "<span class=\"saver\">.<\/span>";
+
+        firstMatch = false;
+
+
+    }
+
+    // clearboard squarereset first squarereset
+
     function clearBoard() {
+
+        console.log("board cleared");
+
+        // clear the player and computer move logs
+        playerTurns = [];
+        computerTurns = [];
+
+        //squareReset();
+
         // reset the squares
         for (var i = 0; i < squares.length; i++){
             squares[i].innerHTML = "<span class=\"saver\">.<\/span>";
             squares[i].classList.remove("wincolor");
         }
 
-        //TODO: DOM travsersal to access first child or something of a saver span and adds the x and o id back
-        // TODO: set CONST of match wins
-
-        // clear the match outcome message
-        message.innerHTML = "Best 2 out of 3 WINS!";        
-
-        // clear the player and computer move logs
-        playerTurns = [];
-        computerTurns = [];
-
-        // if player chose O, computer goes first
-        if (playerSymbol === "O"){
-            setTimeout(computerMove, 1000);
-            gamePlay();
+        if (firstMatch === true){
+            console.log("it's the first match");
+            chooseSymbol();
+            return;
         } else {
-            setTimeout(gamePlay, 500);
+            console.log("it's not the first match");
+            // if player chose O, computer goes first
+            if (playerSymbol === "O"){
+                console.log("Player is O, computer goes first");
+                setTimeout(computerMove, 1000);
+                activateBoard();
+            } else {
+                console.log("Player is X and goes first");
+                setTimeout(activateBoard, 500);
+            }
         }
+        
     }
 
     function checkForWin(){
@@ -199,7 +256,7 @@ $(document).ready(function () {
                             playerScore += 1;
                             playerScoreBoard.innerHTML = playerScore;                                                    
                             // if player has won three matches, display message
-                            if (playerScore === 3){
+                            if (playerScore === GAME_WIN){
                                 message.innerHTML = "<span class=\"game-over flash\">YOU WIN! GAME OVER!<\/span>";
                                 playAgainButton.style.visibility = "visible";
                             } 
@@ -208,7 +265,7 @@ $(document).ready(function () {
                             compScore += 1;
                             compScoreBoard.innerHTML = compScore;                                    
                             // if computer has won three matches, display message
-                            if (compScore === 3){
+                            if (compScore === GAME_WIN){
                                 message.innerHTML = "<span class=\"game-over flash\">YOU LOSE! GAME OVER!<\/span>";
                                 playAgainButton.style.visibility = "visible";
                             }
@@ -241,7 +298,7 @@ $(document).ready(function () {
         move.innerHTML = "<span class=\"comp-color\">" + compSymbol + "<\/span>";
 
         // deactivate that square
-        move.removeEventListener("click", clickEvent);
+        move.removeEventListener("click", playerMove);
 
         // log the move
         computerTurns.push(move.id);
@@ -253,19 +310,7 @@ $(document).ready(function () {
         checkForWin();
     }
     
-    function gamePlay(){
-        //console.log("game is ready to play")
-
-        lockout = false;
-
-        for (var i = 0; i < squares.length; i++) {
-            //console.log("Loop running! i is " + i);
-            //console.log(squares[i]);
-            squares[i].addEventListener("click", clickEvent);
-        } 
-        return true;
-    }
-
+    // Begin the game! 
     playerScoreBoard.innerHTML = playerScore;
     compScoreBoard.innerHTML = compScore;
 
@@ -274,5 +319,7 @@ $(document).ready(function () {
     topL.addEventListener("click", assignX);
     topR.addEventListener("click", assignO);
 
+    playAgainButton.addEventListener("click", playAgain);
+    
 
 }); 
